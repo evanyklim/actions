d4638895846213277945de77377aa8f4bc530bf9
Evan Lim <evanyklim@gmail.com>
2024-06-06 18:17:52 -0400

    add some code

--- actions/src/main.rs
+++ actions/src/main.rs
@@ -1,3 +1,8 @@
+// The `derive` attribute automatically creates the implementation
+// required to make this `struct` printable with `fmt::Debug`.
+#[derive(Debug)]
+struct DebugPrintable(i32);
+
 fn main() {
     println!("Hello, world!");
 }
