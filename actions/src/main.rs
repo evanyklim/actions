96fa47ce5f9bccb6963426a0d5ff8256ecab64d6
Evan Lim <evanyklim@gmail.com>
2024-09-07 16:01:52 -0400

    feat(src): small fix

--- actions/src/main.rs
+++ actions/src/main.rs
@@ -1,9 +1,9 @@
 // The `derive` attribute automatically creates the implementation
 // required to make this `struct` printable with `fmt::Debug`.
-#[derive(Debug)]
-struct DebugPrintable(i32);
+// #[derive(Debug)]
+// struct DebugPrintable(i32);
 
 fn main() {
     println!("Hello, world!");
-    println!("Remember, Github Actions logs are deleted after 90 days")
+    println!("Remember, Github Actions logs are deleted after 90 days");
 }
